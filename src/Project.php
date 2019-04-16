0741b0fe840580870828679198b1e38dd1698570
jhotadhari <tellme@waterproof-webdesign.info>
2019-04-16 17:57:06 +0530

    Remove init code for vendor packages
    Code should be in indivudual single file. Might be added by generator.

--- src/Project.php
+++ src/Project.php
@@ -245,10 +245,6 @@ abstract class Project {
 	}
 
 	public function auto_include() {
-		// init cmb2
-		if ( file_exists( $this->get_dir_path() . 'vendor/cmb2/cmb2/init.php' ) ) {
-			require_once $this->get_dir_path() . 'vendor/cmb2/cmb2/init.php';
-		}
 		// include inc/fun/*.php
 		$this->_include( 'fun' );
 	}
